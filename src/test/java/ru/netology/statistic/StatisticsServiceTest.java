16c17626e04dbba0e1e72d4e5fe6ebfd3e1da411
milalucky <tquestufa@gmail.com>
2025-05-03 20:06:58 +0500

    ex.1 - JaCoCo

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxShouldCoverAllBranches() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {3, 5, 2, 10, 10, 4};
+
+        long expected = 10;
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+}
